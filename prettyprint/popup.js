53c3f8e3c5337879d8a26a234f51d2890be38272
Maurici0 <info@mauricestolk.nl>
2022-03-27 14:09:28 +0200

    Readable js

---
+++ prettyprint/popup.js
@@ -0,0 +1,50 @@
+// popup.js
+const errorHandler = err =>
+{
+	console.log( err );
+
+}
+
+const handler = () =>
+{
+	const iframe = document.body.querySelector(`[data-component="hnschat"]`);
+
+	chrome.storage.sync.get("settings", r =>
+	{
+		const __ = {};
+
+		try
+		{
+			__.w = parseInt( r.settings.width );
+
+			__.h = parseInt( r.settings.height );
+
+			__.s = r.settings.source;
+
+		}
+		catch( err )
+		{
+			__.w = 324;
+
+			__.h = 600;
+
+			__.s = "https://hnschat/";
+
+			chrome.storage.sync.set({"settings": { width: __.w, height: __.h, source: __.s }});
+
+		}
+		
+
+		iframe.onError = errorHandler;
+
+		iframe.width = __.w;
+	
+		iframe.height = __.h;
+	
+		iframe.src = __.s;
+
+	});
+
+}
+
+document.addEventListener("DOMContentLoaded", handler, false );
